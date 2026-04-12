5ba7744e98f0949697fbb1d386fa0b2e964e4a3a
tiko.aqsa@gmail.com <tiko.aqsa@gmail.com>
2026-04-12 12:12:21 +0700

    [v1.0.0-beta.10] Optimized build script

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const nextConfig = {
     experimental: {
         optimizePackageImports: ["@untitledui/icons"],
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
+    }
 };
 
 const withNextIntl = createNextIntlPlugin();
